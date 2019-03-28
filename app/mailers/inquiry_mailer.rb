e21ba7ca503330c3636bb252a8bc9f75cac5ae90
asahitomoi <asatomo1113@yahoo.co.jp>
2019-03-28 19:27:08 +0900

    【update】デプロイ環境でも写真の閲覧可能へ。
    jpeg→jpgに変更したことでデプロイ環境でも写真を見れるようにしました。

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,5 +1,5 @@
 class InquiryMailer < ActionMailer::Base
-  default from: "example@example.com"   # 送信元アドレス
+  default from: "toolpad123@gmail.com"   # 送信元アドレス
   default to: "toolpad123@gmail.com"     # 送信先アドレス
 
   def received_email(inquiry)
